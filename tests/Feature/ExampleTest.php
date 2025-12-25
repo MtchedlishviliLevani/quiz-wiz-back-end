ad47246875d24a075ce0be1fa295dd7c746a9146
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-12-25 16:08:33 +0400

    chore: add PHP CS fixer and format entire project

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,15 +5,20 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+	/**
+	 * A basic test example.
+	 */
+	public function testTheApplicationReturnsASuccessfulResponse(): void
+	{
+		$response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+		$response->assertStatus(200);
+	}
 }
