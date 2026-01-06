862176b9c6a16132c1ef4d066dfbc21e43393944
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-06 19:33:32 +0400

    feat(db): add models with relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -20,7 +20,7 @@ class User extends Authenticatable
 	 * @var list<string>
 	 */
 	protected $fillable = [
-		'name',
+		'username',
 		'email',
 		'password',
 	];
@@ -46,5 +46,15 @@ class User extends Authenticatable
 			'email_verified_at' => 'datetime',
 			'password'          => 'hashed',
 		];
+	}
+
+	public function quizzes(): HasMany
+	{
+		return $this->hasMany(Quiz::class);
+	}
+
+	public function results(): HasMany
+	{
+		return $this->hasMany(QuizResult::class);
 	}
 }
