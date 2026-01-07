80dfe98f1354e32e957a8bdfb1be4f317621d625
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-07 20:57:04 +0400

    feat(auth): add QuizPolicy

---
+++ app/Policies/QuizPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Quiz;
+use App\Models\User;
+
+class QuizPolicy
+{
+	public function viewAny(): bool
+	{
+		return true;
+	}
+
+	public function view(User $user, Quiz $quiz): bool
+	{
+		return $user->id === $quiz->user_id;
+	}
+
+	public function create(): bool
+	{
+		return true;
+	}
+
+	public function update(User $user, Quiz $quiz): bool
+	{
+		return $user->id === $quiz->user_id;
+	}
+
+	public function delete(User $user, Quiz $quiz): bool
+	{
+		return $user->id === $quiz->user_id;
+	}
+}
