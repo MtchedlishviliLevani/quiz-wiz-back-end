60f28ea87a9cc257c1ccd274761534875e7bea7f
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-16 14:21:41 +0400

    feat(auth): add logout and forget password routes with functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ForgotPasswordRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Services\Auth\AuthService;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
 
 class AuthController extends Controller
 {
@@ -55,5 +59,34 @@ class AuthController extends Controller
 	{
 		$request->fulfill();
 		return response()->json(['message' => 'Email verified.']);
+	}
+
+	public function logout(Request $request): JsonResponse
+	{
+		Auth::guard('web')->logout();
+
+		$request->session()->invalidate();
+		$request->session()->regenerateToken();
+
+		return response()->json([
+			'message' => 'You’ve been logged out successfully.',
+		]);
+	}
+
+	public function forgotPassword(ForgotPasswordRequest $request): JsonResponse
+	{
+		$status = Password::sendResetLink(
+			$request->validated()
+		);
+
+		if ($status === Password::RESET_LINK_SENT) {
+			return response()->json([
+				'message' => __($status),
+			], 200);
+		}
+
+		return response()->json([
+			'message' => __($status),
+		], 422);
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,14 @@ Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::middleware('guest:sanctum')->group(function (): void {
+	Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+	Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+	Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('auth.forgot-password');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+	Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
