f1feee694cc56068a6b92e2cd9500387b396d1e4
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-16 18:55:24 +0400

    feat: configure cors and update routes

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ Route::middleware('guest:sanctum')->group(function (): void {
 	Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 	Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 	Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('auth.forgot-password');
+	Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('auth.reset-password');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
