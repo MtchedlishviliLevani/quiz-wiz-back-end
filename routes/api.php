16d837e1e5635386cf84676d4e30dd6b480a0013
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-21 17:46:37 +0400

    feat(route): create route for quiz listing

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+
+Route::get('/quizzes', [QuizController::class, 'index']);
