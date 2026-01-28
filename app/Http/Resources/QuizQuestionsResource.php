aa1d3e668ae8ec15dc85f7b8790eb545d992bc16
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-01-28 12:52:26 +0400

    fix: add types

--- app/Http/Resources/QuizQuestionsResource.php
+++ app/Http/Resources/QuizQuestionsResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Answer;
+use App\Models\Question;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,13 +26,13 @@ class QuizQuestionsResource extends JsonResource
 			'max_points'                  => (int) $this->questions->sum('points'),
 			'play_count'                  => $this->results_count,
 			'quiz_duration'               => $this->duration,
-			'questions'                   => $this->questions->map(fn ($question, $index): array => [
+			'questions'                   => $this->questions->map(fn (Question $question, int $index): array => [
 				'id'            => $question->id,
 				'question'      => $question->question,
 				'number'        => $index + 1,
 				'points'        => $question->points,
 				'correct_count' => $question->answers->where('is_correct', true)->count(),
-				'answers'       => $question->answers->map(fn ($answer): array => [
+				'answers'       => $question->answers->map(fn (Answer $answer): array => [
 					'id'         => $answer->id,
 					'text'       => $answer->text,
 				]),
