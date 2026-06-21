231675d9faaccf6fb2a04e1e999c190ca4428508
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-06-21 13:00:22 +0400

    chore: format route file

--- routes/api.php
+++ routes/api.php
@@ -14,30 +14,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 Route::get('/difficulties', [DifficultyController::class, 'index'])->name('difficulties');
 
 Route::middleware('guest:sanctum')->group(function (): void {
-    Route::post('/login', LoginController::class)->name('auth.login')->middleware('throttle:6,1');
-    Route::post('/register', RegisterController::class)->name('auth.register');
-    Route::post('/forgot-password', ForgotPasswordController::class)->name('auth.forgot-password');
-    Route::post('/reset-password', ResetPasswordController::class)->name('auth.reset-password');
+	Route::post('/login', LoginController::class)->name('auth.login')->middleware('throttle:6,1');
+	Route::post('/register', RegisterController::class)->name('auth.register');
+	Route::post('/forgot-password', ForgotPasswordController::class)->name('auth.forgot-password');
+	Route::post('/reset-password', ResetPasswordController::class)->name('auth.reset-password');
 });
 
 Route::middleware('auth:sanctum')->group(function (): void {
-    Route::post('logout', LogoutController::class);
+	Route::post('logout', LogoutController::class);
 });
 
 Route::get('/email/verify/{id}/{hash}', EmailVerificationController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
 
 Route::prefix('quizzes')->group(function (): void {
-    Route::get('/', [QuizController::class, 'index']);
-    Route::get('/{quiz}', [QuizController::class, 'show']);
-    Route::get('/{quiz}/questions', [QuizController::class, 'QuizQuestions']);
-    Route::post('/submit', [QuizController::class, 'submitQuiz']);
+	Route::get('/', [QuizController::class, 'index']);
+	Route::get('/{quiz}', [QuizController::class, 'show']);
+	Route::get('/{quiz}/questions', [QuizController::class, 'QuizQuestions']);
+	Route::post('/submit', [QuizController::class, 'submitQuiz']);
 });
 
 Route::get('/contacts', [UserContactController::class, 'index'])->name('user-contacts');
